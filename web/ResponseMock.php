0bca5c52ed831765360824a22d7335e608c0cd1e
Novemb3r <igkutyrev@gmail.com>
2018-11-14 21:33:47 +0700

    Don't push this commit

--- web/ResponseMock.php
+++ web/ResponseMock.php
@@ -1,63 +1,65 @@
 <?php
 
-$response = [
-    [
-        'id' => 1,
-        'name' => 'Entrance',
-        'type' => 'Hall',
-        'relatedPoints' => [2,4],
-        'coordinates' => [
-            'x' => 1,
-            'y' => 1
+$building = [
+    'points' => [
+        [
+            'id' => 1,
+            'name' => 'Entrance',
+            'type' => 'Hall',
+            'relatedPoints' => [2, 4],
+            'coordinates' => [
+                'x' => 1,
+                'y' => 1
+            ],
+            'floor' => 0
         ],
-        'floor' => 0
-    ],
-    [
-        'id' => 2,
-        'name' => 'Left hall',
-        'type' => 'Hall',
-        'relatedPoints' => [1,3],
-        'coordinates' => [
-            'x' => 0,
-            'y' => 0
+        [
+            'id' => 2,
+            'name' => 'Left hall',
+            'type' => 'Hall',
+            'relatedPoints' => [1, 3],
+            'coordinates' => [
+                'x' => 0,
+                'y' => 0
+            ],
+            'floor' => 0,
         ],
-        'floor' => 0,
-    ],
-    [
-        'id' => 3,
-        'name' => 'First room',
-        'type' => 'Room',
-        'relatedPoints' => [2],
-        'coordinates' => [
-            'x' => 0,
-            'y' => 1
+        [
+            'id' => 3,
+            'name' => 'First room',
+            'type' => 'Room',
+            'relatedPoints' => [2],
+            'coordinates' => [
+                'x' => 0,
+                'y' => 1
+            ],
+            'floor' => 0,
         ],
-        'floor' => 0,
-    ],
-    [
-        'id' => 4,
-        'name' => 'Right hall',
-        'type' => 'Hall',
-        'relatedPoints' => [1,5],
-        'coordinates' => [
-            'x' => 2,
-            'y' => 0
+        [
+            'id' => 4,
+            'name' => 'Right hall',
+            'type' => 'Hall',
+            'relatedPoints' => [1, 5],
+            'coordinates' => [
+                'x' => 2,
+                'y' => 0
+            ],
+            'floor' => 0,
         ],
-        'floor' => 0,
-    ],
-    [
-        'id' => 5,
-        'name' => 'Left room',
-        'type' => 'Room',
-        'relatedPoints' => [4],
-        'coordinates' => [
-            'x' => 2,
-            'y' => 1
-        ],
-        'floor' => 0,
+        [
+            'id' => 5,
+            'name' => 'Left room',
+            'type' => 'Room',
+            'relatedPoints' => [4],
+            'coordinates' => [
+                'x' => 2,
+                'y' => 1
+            ],
+            'floor' => 0,
+        ]
     ]
 ];
 
-$responseNames = array_map(function ($a) {
-    return ['id' => $a['id'], 'name' => $a['name']];
-}, $response);
+$floorPictures = [
+  0 => 'https://i.imgur.com/7ZsSf.jpg'
+];
--- web/index.php
+++ web/index.php
@@ -7,24 +7,12 @@ $app['debug'] = true;
 
 include './ResponseMock.php';
 
-$app->get('/', function (Silex\Application $app) use ($response) {
-    return $app->json(['points' => $response]);
+$app->get('/building/', function (Silex\Application $app) use ($building) {
+    return $app->json($building);
 });
 
-$app->get('/points/', function (Silex\Application $app) use ($responseNames) {
-    return $app->json(['names' => $responseNames]);
+$app->get('/static/', function (Silex\Application $app) use ($floorPictures) {
+    return $app->json($floorPictures);
 });
-
-$app->get('/points/{id}/', function (Silex\Application $app, $id) use ($response) {
-    return $app->json($response[$id + 1]);
-})
-    ->assert('id', '\d+');
-
-$app->get('/floors/{floorId}/', function (Silex\Application $app, $floorId) use ($response) {
-    return $app->json(array_filter($response, function ($a) use ($floorId) {
-        return ($a['floor'] == $floorId);
-    }));
-})
-    ->assert('floorId', '\d+');
 
 $app->run();
