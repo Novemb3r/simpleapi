6b097713748d06de88f4a2ca15b262fb53c09ea8
Novemb3r <igkutyrev@gmail.com>
2018-11-14 20:01:01 +0700

    FOR REAL.

--- web/index.php
+++ web/index.php
@@ -7,24 +7,24 @@ $app['debug'] = true;
 
 include './ResponseMock.php';
 
-$app->get('/', function(Silex\Application $app) use ($response) {
+$app->get('/', function (Silex\Application $app) use ($response) {
     return $app->json(['points' => $response]);
 });
 
-$app->get('/points/', function(Silex\Application $app) use ($responseNames) {
+$app->get('/points/', function (Silex\Application $app) use ($responseNames) {
     return $app->json(['names' => $responseNames]);
 });
 
-$app->get('/points/{id}/', function(Silex\Application $app, $id) use ($response) {
+$app->get('/points/{id}/', function (Silex\Application $app, $id) use ($response) {
     return $app->json($response[$id + 1]);
 })
-->assert('id', '\d+');
+    ->assert('id', '\d+');
 
-$app->get('/floors/{floorId}/', function(Silex\Application $app, $floorId) use ($response) {
-    return $app->json(array_filter($response, function($a) use ($floorId){
+$app->get('/floors/{floorId}/', function (Silex\Application $app, $floorId) use ($response) {
+    return $app->json(array_filter($response, function ($a) use ($floorId) {
         return ($a['floor'] == $floorId);
     }));
 })
-->assert('floorId', '\d+');
+    ->assert('floorId', '\d+');
 
 $app->run();
