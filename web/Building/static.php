580d5c7526d8c95beb21071175313b53fb20f231
Novemb3r <igkutyrev@gmail.com>
2018-12-11 20:31:33 +0700

    nginx

--- web/Building/static.php
+++ web/Building/static.php
@@ -4,23 +4,23 @@ $floorPictures = [
     'pictures' => [
         [
             'floorId' => 0,
-            'image' => 'https://psv4.userapi.com/c848332/u62878867/docs/d18/20014ce82343/floor_1.png',
+            'image' => 'https://simple-mobile-api.herokuapp.com/static/floor_1.png',
         ],
         [
             'floorId' => 1,
-            'image' => 'https://psv4.userapi.com/c848032/u62878867/docs/d12/c34934c36dd5/floor_2.png',
+            'image' => 'https://simple-mobile-api.herokuapp.com/static/floor_2.png',
         ],
         [
             'floorId' => 2,
-            'image' => 'https://psv4.userapi.com/c848032/u62878867/docs/d13/89a025fcfdfc/floor_3.png',
+            'image' => 'https://simple-mobile-api.herokuapp.com/static/floor_3.png',
         ],
         [
             'floorId' => 3,
-            'image' => 'https://psv4.userapi.com/c848032/u62878867/docs/d4/498cce3338b7/floor_4.png',
+            'image' => 'https://simple-mobile-api.herokuapp.com/static/floor_4.png',
         ],
         [
             'floorId' => 4,
-            'image' => 'https://psv4.userapi.com/c848424/u62878867/docs/d2/d8217d7a4483/floor_5.png',
+            'image' => 'https://simple-mobile-api.herokuapp.com/static/floor_5.png',
         ]
     ]
 ];
